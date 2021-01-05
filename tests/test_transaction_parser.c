14d97235634cfc2b3307b182eb47c12690789161
Thu Cao <Thu.Cao@fdssoft.com>
2021-01-06 06:34:48 +0700

    Update testcases
    - Update name for transaction types
    - Update cosignature transaction test case to show more data
    - Remove mosaic count screen for XYM-only transfer tx
    - Remove 'txs' text in mosaic count screen
    - Add account address/mosaic/operation restriction parser testcase
    - Add delegated harvesting parser testcase
    - Add persistent harvesting delegation transfer parser testcase
    
    Signed-off-by: TriPhan96 <tri.phan@fdssoft.com>

--- tests/test_transaction_parser.c
+++ tests/test_transaction_parser.c
@@ -64,10 +64,9 @@ static void check_transaction_results(const char *filename, int num_fields, cons
 static void test_parse_transfer_transaction(void **state) {
     (void) state;
 
-    const result_entry_t expected[7] = {
+    const result_entry_t expected[6] = {
         {"Transaction Type", "Transfer"},
         {"Recipient", "TDZKL2HAMOWRVEEF55NVCZ7C6GSWIXCI7IWAESI"},
-        {"Mosaics", "Found 1 txs"},
         {"Amount", "45 XYM"},
         {"Message Type", "Plain text"},
         {"Message", "This is a test message"},
@@ -83,7 +82,7 @@ static void test_parse_transfer_transaction_not_xym(void **state) {
     const result_entry_t expected[8] = {
         {"Transaction Type", "Transfer"},
         {"Recipient", "TDZKL2HAMOWRVEEF55NVCZ7C6GSWIXCI7IWAESI"},
-        {"Mosaics", "Found 1 txs"},
+        {"Mosaics", "Found 1"},
         {"Unknown Mosaic", "Divisibility and levy cannot be shown"},
         {"Amount", "45000000 micro 0x5E62990DCAC5B21A"},
         {"Message Type", "Plain text"},
@@ -110,7 +109,7 @@ static void test_parse_create_mosaic(void **state) {
         {"Inner TX Type", "Mosaic Supply Change"},
         {"Mosaic ID", "532CB823113F2471"},
         {"Change Direction", "Increase"},
-        {"Change Amount", "1000000 "},
+        {"Change Amount", "1000000"},
         {"Fee", "2 XYM"},
     };
 
@@ -121,7 +120,7 @@ static void test_parse_create_namespace(void **state) {
     (void) state;
 
     const result_entry_t expected[5] = {
-        {"Transaction Type", "Register Namespace"},
+        {"Transaction Type", "Namespace Registration"},
         {"Namespace Type", "Root namespace"},
         {"Name", "foo576sgnlxdnfbdx"},
         {"Duration", "60d 0h 0m"},
@@ -135,7 +134,7 @@ static void test_parse_create_sub_namespace(void **state) {
     (void) state;
 
     const result_entry_t expected[5] = {
-        {"Transaction Type", "Register Namespace"},
+        {"Transaction Type", "Namespace Registration"},
         {"Namespace Type", "Sub namespace"},
         {"Name", "foo576sgnlxdnfbdx"},
         {"Parent ID", "000000000002A300"},
@@ -148,11 +147,12 @@ static void test_parse_create_sub_namespace(void **state) {
 static void test_parse_supply_change_mosaic(void **state) {
     (void) state;
 
-    const result_entry_t expected[4] = {
+    const result_entry_t expected[5] = {
         {"Transaction Type", "Mosaic Supply Change"},
         {"Mosaic ID", "7CDF3B117A3C40CC"},
         {"Change Direction", "Increase"},
-        {"Change Amount", "1000000 "},
+        {"Change Amount", "1000000"},
+        {"Fee", "2 XYM"}
     };
 
     check_transaction_results("../testcases/supply_change_mosaic.raw", sizeof(expected) / sizeof(expected[0]), expected);
@@ -186,13 +186,63 @@ static void test_parse_link_namespace_to_address(void **state) {
     check_transaction_results("../testcases/link_namespace_to_address.raw", sizeof(expected) / sizeof(expected[0]), expected);
 }
 
+static void test_parse_account_address_restriction(void **state) {
+    (void) state;
+
+    const result_entry_t expected[8] = {
+        {"Transaction Type", "Account Address Restriction"},
+        {"Addition Count", "1 address(es)"},
+        {"Address", "TDZKL2HAMOWRVEEF55NVCZ7C6GSWIXCI7IWAESI"},
+        {"Deletion Count", "Not change"},
+        {"Restriction Flag", "Block"},
+        {"Restriction Flag", "Imcoming"},
+        {"Restriction Flag", "Address"},
+        {"Fee", "0.16 XYM"}
+    };
+
+    check_transaction_results("../testcases/account_address_restriction.raw", sizeof(expected) / sizeof(expected[0]), expected);
+}
+
+static void test_parse_account_mosaic_restriction(void **state) {
+    (void) state;
+
+    const result_entry_t expected[7] = {
+        {"Transaction Type", "Account Mosaic Restriction"},
+        {"Addition Count", "1 mosaic(s)"},
+        {"Mosaic ID", "5BA212858B2B48BC"},
+        {"Deletion Count", "Not change"},
+        {"Restriction Flag", "Block"},
+        {"Restriction Flag", "Mosaic"},
+        {"Fee", "0.144 XYM"}
+    };
+
+    check_transaction_results("../testcases/account_mosaic_restriction.raw", sizeof(expected) / sizeof(expected[0]), expected);
+}
+
+static void test_parse_account_operation_restriction(void **state) {
+    (void) state;
+
+    const result_entry_t expected[8] = {
+        {"Transaction Type", "Account Operation Restriction"},
+        {"Addition Count", "1 operation(s)"},
+        {"Operation Type", "Account Key Link"},
+        {"Deletion Count", "Not change"},
+        {"Restriction Flag", "Block"},
+        {"Restriction Flag", "Outgoing"},
+        {"Restriction Flag", "Transaction Type"},
+        {"Fee", "0.138 XYM"}
+    };
+
+    check_transaction_results("../testcases/account_operation_restriction.raw", sizeof(expected) / sizeof(expected[0]), expected);
+}
+
 static void test_parse_account_multisig(void **state) {
     (void) state;
 
     const result_entry_t expected[10] = {
         {"Transaction Type", "Aggregate Bonded"},
         {"Agg. Tx Hash", "043D6F6E851CAE4ED2B975AEEF61DFDF00B85BBB2503AC23DD7586E3C0B07956"},
-        {"Inner TX Type", "Modify Multisig Account"},
+        {"Inner TX Type", "Multisig Account Modification"},
         {"Address Add Num", "2"},
         {"Address", "TALSLGUUF5VOB2RSWAPDNBUHIBKTNZQREXWPOAI"},
         {"Address", "TBFXGDVDW4TMYEVJ7L3YWTJXGVH7Q4RNXOKQCNY"},
@@ -209,7 +259,7 @@ static void test_parse_hash_lock_account_multisig(void **state) {
     (void) state;
 
     const result_entry_t expected[5] = {
-        {"Transaction Type", "Hash Lock"},
+        {"Transaction Type", "Funds Lock"},
         {"Lock Quantity", "10 XYM"},
         {"Duration", "0d 4h 0m"},
         {"Tx Hash", "2B51EBCBC3E40EFE8AF68A0408F5A72474B1327A64E3E3B47D9B139230C7833B"},
@@ -222,12 +272,11 @@ static void test_parse_hash_lock_account_multisig(void **state) {
 static void test_parse_multisig_transfer_transaction(void **state) {
     (void) state;
 
-    const result_entry_t expected[9] = {
+    const result_entry_t expected[8] = {
         {"Transaction Type", "Aggregate Bonded"},
         {"Agg. Tx Hash", "4941C270B56778E01629FC82EDDC622668F076CE1583AFCCA3F6DE7FE03615BB"},
         {"Inner TX Type", "Transfer"},
         {"Recipient", "TBKQPST7HUOJA2PBNYNA7TT4LLKGA5BB5UY6M4Y"},
-        {"Mosaics", "Found 1 txs"},
         {"Amount", "10 XYM"},
         {"Message Type", "Plain text"},
         {"Message", "Test message"},
@@ -253,7 +302,7 @@ static void test_parse_multisig_create_mosaic(void **state) {
         {"Inner TX Type", "Mosaic Supply Change"},
         {"Mosaic ID", "78CA2F4797C65A64"},
         {"Change Direction", "Increase"},
-        {"Change Amount", "500000000 "},
+        {"Change Amount", "500000000"},
         {"Fee", "0.033696 XYM"},
     };
 
@@ -266,7 +315,7 @@ static void test_parse_multisig_create_namespace(void **state) {
     const result_entry_t expected[7] = {
         {"Transaction Type", "Aggregate Bonded"},
         {"Agg. Tx Hash", "B96E1C08F8434BFDC4D1F292EB3F911B1A3C5B3EE102887A8ACDD75A79A4BB62"},
-        {"Inner TX Type", "Register Namespace"},
+        {"Inner TX Type", "Namespace Registration"},
         {"Namespace Type", "Root namespace"},
         {"Name", "multisig"},
         {"Duration", "60d 0h 0m"},
@@ -280,7 +329,7 @@ static void test_parse_hash_lock_multisig_create_namespace(void **state) {
     (void) state;
 
     const result_entry_t expected[5] = {
-        {"Transaction Type", "Hash Lock"},
+        {"Transaction Type", "Funds Lock"},
         {"Lock Quantity", "10 XYM"},
         {"Duration", "0d 8h 20m"},
         {"Tx Hash", "E019A4A92002505B8B5029AE556958ADCDFBEDAC26C2F79DE1668C5BC588EDF7"},
@@ -294,7 +343,7 @@ static void test_parse_multisig_create_sub_namespace(void **state) {
     (void) state;
 
     const result_entry_t expected[5] = {
-        {"Transaction Type", "Register Namespace"},
+        {"Transaction Type", "Namespace Registration"},
         {"Namespace Type", "Sub namespace"},
         {"Name", "sub_namespace_multisig"},
         {"Parent ID", "D64FAC0976CC0914"},
@@ -307,10 +356,15 @@ static void test_parse_multisig_create_sub_namespace(void **state) {
 static void test_parse_cosignature_transaction(void **state) {
     (void) state;
 
-    const result_entry_t expected[3] = {
+    const result_entry_t expected[8] = {
         {"Transaction Type", "Aggregate Bonded"},
-        {"Agg. Tx Hash", "89F54478AD080E36912701C20DDE49B13A5E8702953C68EA24F5EE2EF068AE0A"},
-        {"Fee", "0.248 XYM"}
+        {"Agg. Tx Hash", "0EFE6E4A881D312984767CABBE53DAC00419E179932A5C784B51132FBE5F7C88"},
+        {"Detail TX Type", "Transfer"},
+        {"Recipient", "TDZKL2HAMOWRVEEF55NVCZ7C6GSWIXCI7IWAESI"},
+        {"Amount", "10 XYM"},
+        {"Message Type", "Plain text"},
+        {"Message", "SDV"},
+        {"Fee", "0.48 XYM"}
     };
 
     check_transaction_results("../testcases/cosignature_transaction.raw", sizeof(expected) / sizeof(expected[0]), expected);
@@ -369,6 +423,41 @@ static void test_parse_namespace_metadata_transaction(void **state) {
     check_transaction_results("../testcases/namespace_metadata_transaction.raw", sizeof(expected) / sizeof(expected[0]), expected);
 }
 
+static void test_parse_delegated_harvesting(void **state) {
+    (void) state;
+
+    const result_entry_t expected[12] = {
+        {"Transaction Type", "Aggregate Complete"},
+        {"Agg. Tx Hash", "0C8666CEF61F61B78515149A1414455C77A0CCD7C9AD5F39DFE46761AB6556DF"},
+        {"Inner TX Type", "Account Key Link"},
+        {"Action", "Link"},
+        {"Linked Acct. PbK", "00278C080D6B149902E1576723DA6362065D3A134BEE6383827353540492B911"},
+        {"Inner TX Type", "Vrf Key Link"},
+        {"Action", "Link"},
+        {"Linked Vrf PbK", "C1A71431325873D83894977C68C783A4EED7A3391EAFD704BF8500361EC321DB"},
+        {"Inner TX Type", "Node Key Link"},
+        {"Action", "Link"},
+        {"Linked Node PbK", "81890592F960AAEBDA7612C8917FA9C267A845D78D74D4B3651AF093E6775001"},
+        {"Fee", "0.0432 XYM"}
+    };
+
+    check_transaction_results("../testcases/delegated_harvesting.raw", sizeof(expected) / sizeof(expected[0]), expected);
+}
+
+static void test_parse_persistent_harvesting_delegation_transfer(void **state) {
+    (void) state;
+
+    const result_entry_t expected[5] = {
+        {"Transaction Type", "Transfer"},
+        {"Recipient", "TBR6AUIUNBRSXJ34RSCZOJTK4GQNVDEUEDCPMIY"},
+        {"Message Type", "Persistent harvesting delegation"},
+        {"Harvesting Message", "FE2A8061577301E28AEC26D42EFCE832BE498BB8CFCC7687BC5BC6B22A82F4BA415A7DF13E1DEA994EAD70125CA250DD6CD8AEA8BAE26AD9A8FC9CB45A996E59BD8894E3D618043887E2383A6BB161A18AB58F406D7DFF384CBD6A669FD152E5AD84B372425212CAAECCB712674AA6C737894BB14FADFE93A3E3AF73A34187D49740891C"},
+        {"Fee", "0.292 XYM"}
+    };
+
+    check_transaction_results("../testcases/persistent_harvesting_delegation_transfer.raw", sizeof(expected) / sizeof(expected[0]), expected);
+}
+
 int main(void) {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(test_parse_transfer_transaction),
@@ -379,6 +468,9 @@ int main(void) {
         cmocka_unit_test(test_parse_supply_change_mosaic),
         cmocka_unit_test(test_parse_link_namespace_to_mosaic),
         cmocka_unit_test(test_parse_link_namespace_to_address),
+        cmocka_unit_test(test_parse_account_address_restriction),
+        cmocka_unit_test(test_parse_account_mosaic_restriction),
+        cmocka_unit_test(test_parse_account_operation_restriction),
         cmocka_unit_test(test_parse_account_multisig),
         cmocka_unit_test(test_parse_hash_lock_account_multisig),
         cmocka_unit_test(test_parse_multisig_transfer_transaction),
@@ -389,7 +481,9 @@ int main(void) {
         cmocka_unit_test(test_parse_cosignature_transaction),
         cmocka_unit_test(test_parse_account_metadata_transaction),
         cmocka_unit_test(test_parse_mosaic_metadata_transaction),
-        cmocka_unit_test(test_parse_namespace_metadata_transaction)
+        cmocka_unit_test(test_parse_namespace_metadata_transaction),
+        cmocka_unit_test(test_parse_delegated_harvesting),
+        cmocka_unit_test(test_parse_persistent_harvesting_delegation_transfer)
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
