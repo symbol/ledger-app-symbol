8430ec4abd22d898616e0e4ca515d73fa9a55384
Jean-Baptiste Bédrune <jean-baptiste.bedrune@ledger.fr>
2021-11-25 15:24:19 +0100

    Fix broken basic int readers and declare them inline

--- src/xym/format/readers.h
+++ src/xym/format/readers.h
@@ -20,12 +20,28 @@
 
 #include <stdint.h>
 
-int8_t read_int8(const uint8_t *src);
-int16_t read_int16(const uint8_t *src);
+static inline int8_t read_int8(const uint8_t *src) {
+    return *(const int8_t *) src;
+}
 
-uint8_t read_uint8(const uint8_t *src);
-uint16_t read_uint16(const uint8_t *src);
-uint16_t read_uint32(const uint8_t *src);
-uint64_t read_uint64(const uint8_t *src);
+static inline int16_t read_int16(const uint8_t *src) {
+    return *(const int16_t *) src;
+}
+
+static inline uint8_t read_uint8(const uint8_t *src) {
+    return *(const uint8_t *) src;
+}
+
+static inline uint16_t read_uint16(const uint8_t *src) {
+    return *(const uint16_t *) src;
+}
+
+static inline uint16_t read_uint32(const uint8_t *src) {
+    return *(const uint32_t *) src;
+}
+
+static inline uint64_t read_uint64(const uint8_t *src) {
+    return *(const uint64_t *) src;
+}
 
 #endif //LEDGER_APP_XYM_READERS_H
